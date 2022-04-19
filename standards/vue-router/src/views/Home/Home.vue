e0cdfd215eae6a25c137360905b0b90062661066
zmtheus <matheus.nsantos@bandtec.com.br>
2022-04-19 07:05:28 -0300

    atualizações

--- standards/vue-router/src/views/Home/Home.vue
+++ standards/vue-router/src/views/Home/Home.vue
@@ -1,7 +1,14 @@
 <template>
     <div>
-        <h1>OI</h1>
-        <button class="btn">Enviar</button>
+        <input type="text" placeholder="Texto" class="mts-input empty"><br>
+        <input type="text" placeholder="Texto" class="mts-input filled"><br>
+        <input type="text" placeholder="Texto" class="mts-input sucess"><br>
+        <input type="text" placeholder="Texto" class="mts-input warning"><br>
+        <input type="text" placeholder="Texto" class="mts-input failed"><br>
+        <button class="mts-button void">matheus</button><br>
+        <button class="mts-button large">matheus</button><br>
+        <button class="mts-button medium">matheus</button><br>
+        <button class="mts-button small">matheus</button><br>
     </div>
 </template>
 
